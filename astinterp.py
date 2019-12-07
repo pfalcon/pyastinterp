ba8a011e6a99dab49cf00f447e220c7f39cba566
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-12-07 14:08:53 +0300

    astinterp: Initial version with BinOp, Call and Assign support.

---
+++ astinterp.py
@@ -0,0 +1,122 @@
+# Python AST interpreter written in Python
+#
+# This module is part of the Pycopy https://github.com/pfalcon/pycopy
+# project.
+#
+# Copyright (c) 2019 Paul Sokolovsky
+#
+# The MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import ast
+import builtins
+
+
+class StrictNodeVisitor(ast.NodeVisitor):
+
+    def generic_visit(self, node):
+        n = node.__class__.__name__
+        raise NotImplementedError("Visitor for node {} not implemented".format(n))
+
+
+class Interpreter(StrictNodeVisitor):
+
+    def __init__(self):
+        self.ns = {}
+        # To implement "store" operation, we need to arguments: location and
+        # value to store. The operation itself is handled by a node visitor
+        # (e.g. visit_Name), and location is represented by AST node, but
+        # there's no support to pass additional arguments to a visitor
+        # (likely, because it would be a burden to explicit pass such
+        # additional arguments thru the chain of visitors). So instead, we
+        # store this value as field. As interpretation happens sequentially,
+        # there's no risk that it will be overwritten "concurrently".
+        self.store_val = None
+
+    def stmt_list_visit(self, lst):
+        for s in lst:
+            self.visit(s)
+
+    def visit_Module(self, node):
+        self.stmt_list_visit(node.body)
+
+    def visit_Expression(self, node):
+        return self.visit(node.body)
+
+    def visit_Assign(self, node):
+        self.store_val = self.visit(node.value)
+        for n in node.targets:
+            self.visit(n)
+
+    def visit_Expr(self, node):
+        # Produced value is ignored
+        self.visit(node.value)
+
+    def visit_Call(self, node):
+        func = self.visit(node.func)
+        args = [self.visit(a) for a in node.args]
+        kwargs = {kw.arg: self.visit(kw.value) for kw in node.keywords}
+        return func(*args, **kwargs)
+
+    def visit_BinOp(self, node):
+        binop_map = {
+            ast.Add: lambda x, y: x + y,
+            ast.Sub: lambda x, y: x - y,
+            ast.Mult: lambda x, y: x * y,
+            ast.Div: lambda x, y: x / y,
+            ast.FloorDiv: lambda x, y: x // y,
+            ast.Mod: lambda x, y: x % y,
+            ast.Pow: lambda x, y: x ** y,
+            ast.LShift: lambda x, y: x << y,
+            ast.RShift: lambda x, y: x >> y,
+            ast.BitAnd: lambda x, y: x & y,
+            ast.BitOr: lambda x, y: x | y,
+            ast.BitXor: lambda x, y: x ^ y,
+        }
+        l = self.visit(node.left)
+        r = self.visit(node.right)
+        return binop_map[type(node.op)](l, r)
+
+    def visit_Name(self, node):
+        if isinstance(node.ctx, ast.Load):
+            if node.id in self.ns:
+                return self.ns[node.id]
+            return getattr(builtins, node.id)
+        elif isinstance(node.ctx, ast.Store):
+            self.ns[node.id] = self.store_val
+        elif isinstance(node.ctx, ast.Del):
+            del self.ns[node.id]
+        else:
+            raise NotImplementedError
+
+    def visit_Str(self, node):
+        return node.s
+
+    def visit_Num(self, node):
+        return node.n
+
+
+if __name__ == "__main__":
+    import sys
+    tree = ast.parse(open(sys.argv[1]).read())
+    #print(ast.dump(tree))
+
+    interp = Interpreter()
+    interp.visit(tree)
